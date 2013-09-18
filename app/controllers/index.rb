61d3d22ede9c90772ac835cd1917b314f10561bd
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-18 18:01:17 -0500

    added password validation

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,46 @@
+
+enable :sessions
+
 get '/' do
   # Look in app/views/index.erb
   erb :index
+end
+
+get '/private' do
+  if session[:message] == "logged_in"
+    erb :private
+  else
+    redirect to('/')
+  end
+
+end
+
+
+post '/login' do
+  # receive the user's input
+  # if valid
+  # => create user session
+  # => redirect to /private
+  # else
+  # => send them back home, saying they had an invalid login
+  if User.authenticate(params[:email], params[:password])
+    session[:message] = "logged_in"
+    redirect to('/private')
+  else
+    @message = "Email or Password invalid"
+    erb :index
+  end
+end
+
+post '/create' do
+  if User.create(params[:info]).valid?
+    # create a user session
+    # then send them to the private page
+    session[:message] = "logged_in"
+    redirect to('/private')
+  else
+    @message = "That email already exists"
+    erb :index
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ActiveRecord::Base
   # Remember to create a migration!
+  validates :email, { presence: true, uniqueness: true }
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
+  def self.authenticate(email, password)
+    user = User.find_by_email(email)
+    (user) && (user.password == password)
+  end
+
 end
